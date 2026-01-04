7dad78a2c5f409f6e77c7b65d2316e3cfe6936d1
syedmuhammadali-dev <laptopboy1126@gmail.com>
2026-01-04 18:07:23 +0500

    5 api created

--- making-api/server.js
+++ making-api/server.js
@@ -7,6 +7,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { config } from "dotenv";
+import users from "./src/dummy-data/dummy-data.js";
 
 // Environment variables config...!
 config({
@@ -16,6 +17,7 @@ config({
 // Global variables...!
 const port = process.env.PORT;
 const app = express();
+let todoBucket = []; // This is a dummmy data base
 
 // Middlewares...!
 app.use(express.json());
@@ -23,12 +25,102 @@ app.use(morgan("dev"));
 app.use(cors());
 
 // Create 1st api: / route...!
-// http://localhost:3000/
 app.get("/", (req, res) => {
   return res.status(200).send({
     statusCode: 200,
     message: "Welcome to Back End using Node JS",
   });
+});
+
+// Create 2nd api: /users route(Feth all users)...!
+app.get("/users", (req, res) => {
+  try {
+    // 400:
+    if (users.length < 1) {
+      return res.status(400).send({
+        status: false,
+        message: "No data found",
+      });
+    }
+
+    // 200:
+    return res.status(200).send({
+      status: true,
+      message: "Users list fetched successfully",
+      data: users,
+    });
+  } catch (error) {
+    // 500:
+    return res.status(500).send({
+      status: false,
+      message: "Something went wrong from server side",
+    });
+  }
+});
+
+// Create 3rd api: /todo/add route(Send todo data to DB)...!
+app.post("/todo/add", (req, res) => {
+  const { todoValue } = req?.body;
+  console.log(`Body data: ${todoValue}`);
+
+  try {
+    // 400:
+    if (!todoValue) {
+      return res.status(400).send({
+        status: false,
+        message: "Todo value is required.",
+      });
+    }
+
+    // 200:
+    const fetchTodos = [...todoBucket];
+    fetchTodos.push(todoValue);
+    todoBucket = fetchTodos;
+
+    return res.status(200).send({
+      status: true,
+      message: "Todo added successfully",
+    });
+  } catch (error) {
+    // 500:
+    return res.status(500).send({
+      status: false,
+      message: "Server us not working!",
+    });
+  }
+});
+
+// Create 4th api: /todo/fetch-all route(Fetch all todo data from DB)...!
+app.get("/todo/fetch-all", (req, res) => {
+  try {
+    // 400:
+    if (todoBucket.length < 1) {
+      return res.status(400).send({
+        status: false,
+        message: "No data found",
+      });
+    }
+
+    // 200:
+    const todos = [...todoBucket];
+    return res.status(200).send({
+      status: true,
+      message: "Todo list fetched successfully",
+      data: todos,
+    });
+  } catch (error) {
+    // 500:
+    return res.status(500).send({
+      status: false,
+      message: "Something went wrong from server side",
+    });
+  }
+});
+
+// Create 5th api: /todo/delete/:key route(Delete todo data from DB)...!
+app.delete("/todo/delete/:key", (req, res) => {
+  const { key } = req.params;
+  console.log(`Todo iten key: ${key}`);
 });
 
 // Server running...!
